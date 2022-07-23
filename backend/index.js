4f03ff39210c03f130e35ad2d6e2d122b4d5f5e6
nananghidayatulloh <nananghidayatulloh@gmail.com>
2022-07-23 08:33:29 +0700

    first endpoint api

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+/*
+ API
+*/
+
+app.get("/api",(req,res)=>{
+    res.json({message: "Hello Bos Ini sample api ya"});
+});
 app.listen(PORT,()=>{
     console.log(`Server jalan di port ${PORT}`);
 })
